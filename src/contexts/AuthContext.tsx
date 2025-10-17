4ad37b528abb441e5cf0a5c749cd3fe1cadbae41
rmumtaz <97579047+rmumtaz@users.noreply.github.com>
2025-10-17 09:56:28 +0500

    when no session go to index page

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -44,6 +44,8 @@ export const AuthProvider = ({ children }) => {
           navigate("/dashboard");
         }
         // navigate("/dashboard");
+      } else {
+        navigate("/");
       }
     });
     const {
